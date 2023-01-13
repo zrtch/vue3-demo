e59681575b5324ab5ed9e34cdcedddeb4b6890be
zrtch <1160639597@qq.com>
2023-01-13 15:57:43 +0800

    首页cocker布局

--- jingdong/src/App.vue
+++ jingdong/src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="docker">
+    <span class="docker_item docker_item--active">
+      <div class="iconfont">&#xe7d4;</div>
+      <div class="docker_title">首页</div>
+    </span>
+    <span class="docker_item">
+      <div class="iconfont">&#xe6e6;</div>
+      <div class="docker_title">购物车</div>
+    </span>
+    <span class="docker_item">
+      <div class="iconfont">&#xe643;</div>
+      <div class="docker_title">订单</div>
+    </span>
+    <span class="docker_item"
+      ><div class="iconfont">&#xe61a;</div>
+      <div class="docker_title">我的</div>
+    </span>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  position: absolute;
+  box-sizing: border-box;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border: 1px solid #f1f1f1;
+  display: flex;
+  &_item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.2rem;
     }
+
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &_title {
+    font-size: 20px;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
   }
 }
 </style>
--- jingdong/src/router/index.js
+++ jingdong/src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
