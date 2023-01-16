713c01989b440ddd2463be3f094a01ed369124c8
zrtch <1160639597@qq.com>
2023-01-16 11:10:56 +0800

    子元素左右留白处理

--- jingdong/src/App.vue
+++ jingdong/src/App.vue
@@ -17,6 +17,13 @@
         alt=""
       />
     </div>
+    <div class="icons">
+      <div class="icons_item" v-for="(item, index) in list" :key="index">
+        <img class="icons_item_img" :src="item.src" alt="" />
+        <p class="icons_item_desc">{{ item.name }}</p>
+      </div>
+    </div>
+    <div class="gap"></div>
   </div>
   <div class="docker">
     <span class="docker_item docker_item--active">
@@ -37,6 +44,58 @@
     </span>
   </div>
 </template>
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const list = reactive([
+      {
+        name: "超市便利",
+        src: "http://www.dell-lee.com/imgs/vue3/超市.png",
+      },
+      {
+        name: "菜市场",
+        src: "http://www.dell-lee.com/imgs/vue3/菜市场.png",
+      },
+      {
+        name: "水果店",
+        src: "http://www.dell-lee.com/imgs/vue3/水果店.png",
+      },
+      {
+        name: "鲜花绿植",
+        src: "http://www.dell-lee.com/imgs/vue3/鲜花.png",
+      },
+      {
+        name: "医药健康",
+        src: "http://www.dell-lee.com/imgs/vue3/医药健康.png",
+      },
+      {
+        name: "家具时尚",
+        src: "http://www.dell-lee.com/imgs/vue3/家居.png",
+      },
+      {
+        name: "烘焙蛋糕",
+        src: "http://www.dell-lee.com/imgs/vue3/蛋糕.png",
+      },
+      {
+        name: "签到",
+        src: "http://www.dell-lee.com/imgs/vue3/签到.png",
+      },
+      {
+        name: "大牌免运",
+        src: "http://www.dell-lee.com/imgs/vue3/大牌免运.png",
+      },
+      {
+        name: "红包套餐",
+        src: "http://www.dell-lee.com/imgs/vue3/红包.png",
+      },
+    ]);
+    return {
+      list,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 @import "./style/viriables.scss";
@@ -89,6 +148,30 @@
     width: 100%;
   }
 }
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 0.16rem;
+  &_item {
+    width: 20%;
+    &_img {
+      display: block;
+      margin: 0 auto;
+      width: 0.4rem;
+      height: 0.4rem;
+    }
+    &_desc {
+      margin: 0.06rem 0 0.16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  height: 0.1rem;
+  background: $content-bgColor;
+  margin: 0 -0.18rem;
+}
 .docker {
   position: absolute;
   box-sizing: border-box;
@@ -97,7 +180,7 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border: 0.01rem solid #f1f1f1;
+  border-top: 0.01rem solid $content-bgColor;
   display: flex;
   color: $content-fontcolor;
   &_item {
@@ -107,7 +190,6 @@
       margin: 0.07rem 0 0.02rem 0;
       font-size: 0.2rem;
     }
-
     &--active {
       color: #1fa4fc;
     }
